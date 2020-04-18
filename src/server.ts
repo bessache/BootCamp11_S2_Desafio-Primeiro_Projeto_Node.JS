26aef456f2a2af4b7ff6552e9ca80ca31852c602
Renato Bessa <bessache@outlook.com>
2020-04-18 11:22:25 -0300

    commit configuracao typeORM

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,12 @@
-import app from './app';
+import express from 'express';
+import routes from './routes';
+import './database';
+
+const app = express();
+
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333, () => {
-  console.log('🚀 Server started on port 3333!');
+  console.log('Server start on port 3333!');
 });
